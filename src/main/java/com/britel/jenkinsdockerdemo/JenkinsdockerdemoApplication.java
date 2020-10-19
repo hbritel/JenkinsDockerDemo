03cb291fd66595e1d3106628a4706184a17db03b
hbritel <britel.hicham.1@gmail.com>
2020-10-19 22:34:32 +0200

    first commit

---
+++ src/main/java/com/britel/jenkinsdockerdemo/JenkinsdockerdemoApplication.java
@@ -0,0 +1,13 @@
+package com.britel.jenkinsdockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsdockerdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsdockerdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/britel/jenkinsdockerdemo/JenkinsdockerdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.britel.jenkinsdockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsdockerdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
